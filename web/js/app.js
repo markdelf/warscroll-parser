60540a67996350f55341a06611d73a4ae7377f7a
Mark Camilleri <mark.camilleri@preeostudios.com>
2016-07-12 17:07:50 +0200

    Late night work - ability to edit points etc

--- web/js/app.js
+++ web/js/app.js
@@ -1,15 +1,22 @@
 var app = angular.module("sampleApp", ["firebase"]);
 app.controller("SampleCtrl", function($scope, $firebaseAuth, $firebaseArray) {
-  var ref = firebase.database().ref();
+  var warscrollsRef = firebase.database().ref('/warscrolls');
+  var unitTypesRef = firebase.database().ref('/unit-types');
 
   var auth = $firebaseAuth();
 
   $scope.doLogin = function() {
     auth.$signInWithPopup("google").then(function(firebaseUser) {
-        $scope.warscrolls = $firebaseArray(ref);
+        $scope.warscrolls = $firebaseArray(warscrollsRef);
         $scope.currentUser = firebaseUser;
+        $scope.unitTypes = $firebaseArray(unitTypesRef);
       }).catch(function(error) {
         console.log("Authentication failed:", error);
       });
+  };
+
+  $scope.selectWarscroll = function(warscroll) {
+    $scope.selectedWarscroll = warscroll;
+    $('#warscrollModal').modal('show');
   };
 });
